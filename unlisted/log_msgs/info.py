ad19cce439fb6fbc73a2b691347acb35fea1b14c
Aditya Pamar <aditsingh8077@gmail.com>
2026-04-30 11:13:09 -0700

    Update info.py

--- unlisted/log_msgs/info.py
+++ unlisted/log_msgs/info.py
@@ -1,7 +1,6 @@
 # ------------- info messages ---------------
 # NOTE: Please do not change any of the following
-# info messages unless their was a typo or some
-# of them where hard to understand by the users
+
 
 CHECKING_CHANNEL_URL = lambda channel_url: f"[bold white]Checking channel URL [bold green]'{channel_url}'[bold white]..."
 CHANNEL_URL_IS_NOT_VALID = lambda channel_url: f"[bold red][ - ] [bold white]Invalid channel URL [bold yellow]{channel_url}[bold white]. Please verify the channel URL and try again."
